9d606b3309f4f9da6593a8daf42a1c27f3f420df
yashdondal <yashdondal21@gmail.com>
2022-10-08 21:21:51 +0530

    greets

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace BasicCodeProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //FlipCoin.Percentage();
+            // LeapYear.CheckingLeap();
+            //HarmonicNumbers.Value();
+            //Vowel.CheckTheTypeOfCharacter();
+            //SwapNumber.SwapingNumber();
+            //LargestNumber.CheckLargestNumber();
+            //OddEven.CheckOddEven();
+            QuotientRemainder.CheckQuotient();
+        }
+    }
+}
