16b7debe7ec3bd83f83ae02a961bc7a4840abbf1
yashdondal <yashdondal21@gmail.com>
2022-10-08 22:08:50 +0530

    To Swap To Number

---
+++ SwapNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCodeProgram
+{
+    public static class SwapNumber
+    {
+        public static void SwapingNumber()
+        {
+            int a=5, b=10;
+
+            Console.WriteLine("Before swap a = " + a + " b = " + b);
+
+            a = a * b;
+            b = a / b;  
+            a = a / b;
+            Console.WriteLine("After Swap a = " + a + " b = " + b ); 
+        }
+    }
+}
