7334172de182bb3405b8921448ddd85e084c0e52
yashdondal <yashdondal21@gmail.com>
2022-10-08 22:13:06 +0530

    VOWELS

---
+++ Vowel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCodeProgram
+{
+    public static class Vowel
+    {
+        public static void CheckTheTypeOfCharacter()
+        {
+            char ch;
+            Console.WriteLine("Enter any character : ");
+            ch = Convert.ToChar(Console.ReadLine());
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine(ch + " is Vowel ");
+            }
+            else
+            {
+                Console.WriteLine(ch + " is Not Vowel ");
+            }
+           
+        }
+    }
+}
