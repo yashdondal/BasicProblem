38a48c95bea176547757516807596caf8fc9c659
yashdondal <yashdondal21@gmail.com>
2022-10-08 22:03:47 +0530

    Harmonic Number

---
+++ HarmonicNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCodeProgram
+{
+    internal class HarmonicNumbers
+    {
+        public static void Value()
+        {
+            int i, n;
+
+            Console.WriteLine("Print The Harmonic Value");
+            n = Convert.ToInt32(Console.ReadLine());
+            double sum = 0;
+            for (i = 1; i <= n; i++)
+            {
+                Console.WriteLine("1/{0}" + i);
+                sum += 1 / (float)i;
+            }
+            Console.Write("\nsum of series upto {0} terms : {1} \n", n, sum);
+        }
+
+    }
+}
