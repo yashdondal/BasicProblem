738fec32aa9190abe567e55b17d4f6f861d4dcab
yashdondal <yashdondal21@gmail.com>
2022-10-08 22:11:35 +0530

    ODD OR EVEN

---
+++ OddEven.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCodeProgram
+{
+    public static class OddEven
+    {
+        public static void CheckOddEven()
+        {
+            int i;
+            Console.Write("Enter a Number : ");
+            i = Convert.ToInt32(Console.ReadLine());
+            if (i % 2 == 0)
+                Console.WriteLine(i + " is even ");
+            else
+                Console.WriteLine(i + "is odd ");
+            Console.ReadKey();   
+        }
+    }
+}
