ef05ca0a0ecec6781b1c147669d333fd3dada0e4
yashdondal <yashdondal21@gmail.com>
2022-10-08 22:05:07 +0530

    To Find Factors

---
+++ Factors.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCodeProgram
+{
+    public  class Factors
+    {
+        int n;
+        public static void factors()
+        {
+            Console.WriteLine("enter any number");
+             int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i <= n; i++)
+            {
+                while (n % i == 0)
+                {
+                    Console.WriteLine(i + " ");
+                    n = n / i;
+                }
+            }
+            if (n >= 2)
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+    }
+}
